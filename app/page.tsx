da171baf46bdc41391625c36b8727f9edbabd1f1
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-16 11:45:10 -0400

    Ensured card is responsive on mobile, tablet and large screens.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { frontend, backend } from "@/data/content.json"
+import Link from "next/link";
 
 export default function page() {
   return (
@@ -17,9 +19,13 @@ export default function page() {
             <h1 className="text-xl/7 font-bold">Front-End Tooling</h1>
           </Card>
           {frontend.map((tools, index) => (
-            <Card key={index} className="col-span-3 p-5 rounded-[0.45rem]">
+            <Card key={index} className="col-span-12 md:col-span-6 xl:col-span-3 p-5 rounded-[0.45rem]">
               <p className="text-md/7 font-bold capitalize">{tools.title}</p>
               <p className="text-sm/5 tracking-wide text-gray-800 line-clamp-2">{tools.description}</p>
+              <hr className="block mt-4" />
+              <Link href={`${tools.url}`} target="_blank" className="block text-sm text-gray-800 hover:underline hover:cursor-pointer mt-4">
+                Visit Website
+              </Link>
             </Card>
           ))}
         </section>
@@ -28,9 +34,13 @@ export default function page() {
             <h2 className="text-xl/7 font-bold">Back-End Tooling</h2>
           </Card>
           {backend.map((tools, index) => (
-            <Card key={index} className="col-span-3 p-5 rounded-[0.45rem]">
+            <Card key={index} className="col-span-12 md:col-span-6 xl:col-span-3 p-5 rounded-[0.45rem]">
               <p className="text-md/7 font-bold capitalize">{tools.title}</p>
-              <p className="text-sm/5 tracking-wide text-gray-800 line-clamp-2">{tools.description}</p>
+              <p className="text-sm/5 tracking-wide text-gray-800 line-clamp-2 mt-2">{tools.description}</p>
+              <hr className="block mt-4" />
+              <Link href={`${tools.url}`} target="_blank" className="block text-sm text-gray-800 hover:underline hover:cursor-pointer mt-4">
+                Visit Website
+              </Link>
             </Card>
           ))}
         </section>
@@ -39,9 +49,13 @@ export default function page() {
             <h3 className="text-xl/7 font-bold">Business Tooling</h3>
           </Card>
           {Array.from({ length: 8 }, (_, index) => (
-            <Card key={index} className="col-span-3 p-5 rounded-[0.45rem]">
+            <Card key={index} className="col-span-12 md:col-span-6 xl:col-span-3 p-5 rounded-[0.45rem]">
               <p className="text-md/7 font-bold capitalize">Some random content.</p>
-              <p className="text-sm/5 tracking-wide text-gray-800 line-clamp-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste enim cumque veniam magni placeat voluptatem dolorem, similique fugit saepe. Vitae, provident? Soluta blanditiis voluptatibus maiores veniam! Ratione autem sunt laboriosam.</p>
+              <p className="text-sm/5 tracking-wide text-gray-800 line-clamp-2 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste enim cumque veniam magni placeat voluptatem dolorem, similique fugit saepe. Vitae, provident? Soluta blanditiis voluptatibus maiores veniam! Ratione autem sunt laboriosam.</p>
+              <hr className="block mt-4" />
+              <Link href={`https://www.justin-bento.com`} className="block text-sm text-gray-800 hover:underline hover:cursor-pointer mt-4">
+                Visit Website
+              </Link>
             </Card>
           ))}
         </section>
