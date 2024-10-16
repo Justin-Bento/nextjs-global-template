dad74a2dd5388e154d26ea138c56781f72b596cd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-16 11:13:58 -0400

    Changed the title of each section for the website

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ export default function page() {
           </div>
         </div>
       </header>
-      <main className="min-h-dvh flex flex-col items-center justify-start container mx-auto mt-10 gap-10">
+      <main className="min-h-dvh flex flex-col items-center justify-start container mx-auto my-10 gap-10">
         <section className="grid grid-cols-1 md:grid-cols-12 gap-4">
           <Card className="col-span-12 p-6">
-            <h1 className="text-xl/7 font-bold">Main Content</h1>
+            <h1 className="text-xl/7 font-bold">Front-End Tooling</h1>
           </Card>
           {Array.from({ length: 8 }, (_, index) => (
             <Card key={index} className="col-span-3 p-5 rounded-[0.45rem]">
@@ -24,7 +24,18 @@ export default function page() {
         </section>
         <section className="grid grid-cols-1 md:grid-cols-12 gap-4">
           <Card className="col-span-12 p-6">
-            <h1 className="text-xl/7 font-bold">Main Content</h1>
+            <h2 className="text-xl/7 font-bold">Back-End Tooling</h2>
+          </Card>
+          {Array.from({ length: 8 }, (_, index) => (
+            <Card key={index} className="col-span-3 p-5 rounded-[0.45rem]">
+              <p className="text-md/7 font-bold capitalize">Some random content.</p>
+              <p className="text-sm/5 tracking-wide text-gray-800 line-clamp-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste enim cumque veniam magni placeat voluptatem dolorem, similique fugit saepe. Vitae, provident? Soluta blanditiis voluptatibus maiores veniam! Ratione autem sunt laboriosam.</p>
+            </Card>
+          ))}
+        </section>
+        <section className="grid grid-cols-1 md:grid-cols-12 gap-4">
+          <Card className="col-span-12 p-6">
+            <h3 className="text-xl/7 font-bold">Business Tooling</h3>
           </Card>
           {Array.from({ length: 8 }, (_, index) => (
             <Card key={index} className="col-span-3 p-5 rounded-[0.45rem]">
