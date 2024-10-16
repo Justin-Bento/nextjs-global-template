e3778fb1b40dc88267a4aaea5f0a7c780f301317
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-16 11:00:54 -0400

    removed blue background and added shadow

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function page() {
   return (
     <>
-      <header className="">
+      <header className="shadow">
         <div className="overflow-hidden">
           <div className="px-4 py-5 sm:p-6 flex items-center justify-start">
             <p className="text-base/7 font-medium">Header Content</p>
           </div>
         </div>
       </header>
-      <main className="min-h-dvh flex items-center justify-start bg-blue-500">
+      <main className="min-h-dvh">
         <div className="px-4 py-5 sm:p-6 flex items-center justify-start">
           <h1 className="text-base/7 font-medium">Main Content</h1>
         </div>
