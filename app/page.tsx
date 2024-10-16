53040b0639a72b491867105124a8ce4a73c38568
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-16 11:08:23 -0400

    Created a bento grid that include a title and description for each section.

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,18 @@ export default function page() {
           </div>
         </div>
       </header>
-      <main className="min-h-dvh">
-        <section className="">
-          <Card>
-            <h1 className="text-base/7 font-medium">Main Content</h1>
+      <main className="min-h-dvh container mx-auto mt-10">
+        <section className="grid grid-cols-1 md:grid-cols-12 gap-4">
+          <Card className="col-span-12 p-6">
+            <h1 className="text-xl/7 font-bold">Main Content</h1>
           </Card>
+          {Array.from({ length: 8 }, (_, index) => (
+            <Card key={index} className="col-span-3 p-5 rounded-[0.45rem]">
+              <p className="text-md/7 font-bold capitalize">Some random content.</p>
+              <p className="text-sm/5 tracking-wide text-gray-800 line-clamp-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste enim cumque veniam magni placeat voluptatem dolorem, similique fugit saepe. Vitae, provident? Soluta blanditiis voluptatibus maiores veniam! Ratione autem sunt laboriosam.</p>
+            </Card>
+          ))}
+
         </section>
       </main>
       <footer className="">
