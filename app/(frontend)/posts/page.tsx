b03726abdf2cff09daaa719ec4e03ba7adc4ca7b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-23 11:18:11 -0400

    Fetch Sanity Content on posts page

--- app/(frontend)/posts/page.tsx
+++ app/(frontend)/posts/page.tsx
@@ -1,9 +1,24 @@
 import Link from "next/link";
+import { client } from "@/sanity/lib/client";
+import { POST_QUERY } from "@/lib/quires";
+
+const options = { next: { revalidate: 60 } };
 
 export default async function Page() {
+  const posts = await client.fetch(POST_QUERY, {}, options);
   return (
     <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <h1 className="text-4xl font-bold">Hello, Blog Posts.</h1>
+      <ul className="grid grid-cols-1 divide-y divide-blue-100">
+        {posts.map((post) => (
+          <li key={post._id}>
+            <Link className="block p-4 hover:text-blue-500" href={`/posts/${post?.slug?.current}`}>
+              {post?.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <hr />
       <hr />
       <Link href="/posts">Posts index &rarr;</Link>
     </section>
