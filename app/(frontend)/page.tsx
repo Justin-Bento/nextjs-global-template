33ece5d7f38c18a151c22b3412acb24d251fc8d4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-23 11:16:24 -0400

    Updated the headings for the pages

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default async function Page() {
   return (
     <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      <h1 className="text-4xl font-bold">Home</h1>
+      <h1 className="text-4xl font-bold">Hello, Home.</h1>
       <hr />
       <Link href="/posts">Posts index &rarr;</Link>
     </section>
