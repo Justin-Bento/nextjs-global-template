d90805399753406f69f422b7250472686f87f466
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-23 12:04:18 -0400

    Enabled images from sanity and unsplash to sanity domains.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "cdn.sanity.io",
+      // ...
+    ],
+  },
+};
 
 export default nextConfig;
