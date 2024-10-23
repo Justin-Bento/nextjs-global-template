def8cae5ff3490eeb53212fef0e7302b34867fce
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-23 11:39:52 -0400

    Updated Type Definitions

--- app/(frontend)/posts/page.tsx
+++ app/(frontend)/posts/page.tsx
@@ -4,13 +4,19 @@ import { POST_QUERY } from "@/lib/quires";
 
 const options = { next: { revalidate: 60 } };
 
+type PostArticle = {
+  _id: string;
+  slug: { current: string };
+  title: string;
+};
+
 export default async function Page() {
   const posts = await client.fetch(POST_QUERY, {}, options);
   return (
     <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <h1 className="text-4xl font-bold">Hello, Blog Posts.</h1>
       <ul className="grid grid-cols-1 divide-y divide-blue-100">
-        {posts.map((post) => (
+        {posts.map((post: PostArticle) => (
           <li key={post._id}>
             <Link className="block p-4 hover:text-blue-500" href={`/posts/${post?.slug?.current}`}>
               {post?.title}
@@ -18,7 +24,6 @@ export default async function Page() {
           </li>
         ))}
       </ul>
-      <hr />
       <hr />
       <Link href="/posts">Posts index &rarr;</Link>
     </section>
