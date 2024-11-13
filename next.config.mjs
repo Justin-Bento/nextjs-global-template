eaab0113823e28f86c05d42f7ec18b2e256cf0b9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-11-12 23:58:20 -0500

    Updated nextconfig to enable remote images from sanity

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    domains: [
-      "images.unsplash.com",
-      "cdn.sanity.io",
-      // ...
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
     ],
   },
 };
